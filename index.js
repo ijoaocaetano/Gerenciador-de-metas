0f2aa2aa28f6b5c9e2385a3cb0de3abdf4d12ad4
Caetano <caetano18.joao@gmail.com>
2024-09-14 20:42:28 -0300

    resolve problema marcadas/desmarcas

--- index.js
+++ index.js
@@ -30,14 +30,14 @@ const listarMeta = async () => {
         instructions: false,
     })
 
+    metas.forEach((m) => {
+        m.checked = false
+    })
+
     if(respostas.length == 0) {
         console.log('Nenhuma meta selecionada!');
         return;
     }
-
-    metas.forEach((m) => {
-        m.checked = false
-    })
 
     respostas.forEach((resposta) => {
         const meta = metas.find((m) => {
