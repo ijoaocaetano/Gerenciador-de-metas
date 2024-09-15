9920e45624c37e1fa9885ccb8cc3e4af41d119a0
Caetano <caetano18.joao@gmail.com>
2024-09-14 21:10:32 -0300

    adiciona metas abertas

--- index.js
+++ index.js
@@ -62,8 +62,24 @@ const metasRealizadas = async () => {
     }
 
     await select ({
-        message: "Metas realizadas",
+        message: "Metas realizadas " + realizadas.length,
         choices: [...realizadas]
+    });
+}
+
+const metasAbertas = async () => {
+    const abertas = metas.filter((meta) => {
+        return meta.checked != true
+    });
+
+    if(abertas.length == 0) {
+        console.log("Não existem metas abertas! :)");
+        return
+    }
+
+    await select ({
+        message: "Metas abertas " + abertas.length,
+        choices: [...abertas]
     });
 }
 
@@ -85,6 +101,10 @@ const start = async () => {
                 {
                     name: "Metas realizadas",
                     value: "realizadas"
+                },
+                {
+                    name: "Metas abertas",
+                    value: "abertas"
                 }, 
                 {
                     name: "Sair", // Vai mostrar sair
@@ -104,6 +124,9 @@ const start = async () => {
                 break;
             case 'realizadas':
                 await metasRealizadas();
+                break;
+            case 'abertas':
+                await metasAbertas();
                 break;
             case 'sair':
                 console.log('Até a próxima!');
